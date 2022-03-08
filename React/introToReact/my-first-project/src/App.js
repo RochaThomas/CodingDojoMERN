1640d53b2207d5a0f312375ced82e943a33b15a8
Tommy Rocha <tomrocha99@gmail.com>
2022-03-08 14:34:45 -0800

    first react with JSX complete

---
+++ React/introToReact/my-first-project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MyFirstComponent from './components/myFirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyFirstComponent/>
+    </div>
+  );
+}
+export default App;
+
+
